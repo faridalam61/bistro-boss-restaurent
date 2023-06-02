68c6fce537ea6d0eef0c29f2ac8fb56db1a14c85
Farid Alam <faridalam@Farids-MacBook-Air.local>
2023-06-02 17:33:42 +0600

    added add to cart

--- src/Pages/Order/ProductCard.jsx
+++ src/Pages/Order/ProductCard.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ item }) {
-  const { name, image, price, recipe } = item;
+  const { name, image, price, recipe,_id } = item;
+  const navigate = useNavigate()
+  const {user} = useContext(AuthContext)
+  const handleAddtoCart = (name)=>{
+    if(!user){
+      Swal.fire({
+      title: 'You must login first',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Login'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+          navigate('/login')
+        
+      }
+    })
+    }else{
+      const cartItem = {foodId:_id,name,image,price,recipe,email:user.email}
+      fetch('http://localhost:5000/carts',{
+        method:'POST',
+        headers:{
+          "content-type":"application/json"
+        },
+        body:JSON.stringify(cartItem)
+      })
+      .then(res => res.json())
+      .then(result =>{
+        if(result.insertedId){
+          Swal.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: 'Product added to cart',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          
+        }
+      })
+    }
+  }
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -12,7 +57,7 @@ function ProductCard({ item }) {
         <p>{recipe}</p>
         <p className="text-xl font-bold">${price}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary w-full">Buy Now</button>
+          <button onClick={()=>handleAddtoCart(name)} className="btn btn-primary w-full">Add to Cart</button>
         </div>
       </div>
     </div>
